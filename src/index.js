497c4f6c8c7845a29164545de810f33770ae9b1a
Julian Motz <me@julianmotz.com>
2017-04-29 20:07:32 +0200

    Show title, assigned to and status as reply

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const SlackBot = require("slackbots"),
-    express = require("express");
+  express = require("express"),
+  request = require("request");
 
 // Init instances
 const bot = new SlackBot({
@@ -8,27 +9,42 @@ const bot = new SlackBot({
   }),
   app = express();
 
-// settings
-const redmineUrl = "https://redmine.cando-image.com/issues/",
-  chkRegExp = /#[0-9]{4,10}/gmi,
-  port = process.argv.slice(2)[0];
-
-// listen on slack messages
+// Listen on Slack messages
 bot.on("message", function(data) {
-  if(data.type === "message" && typeof data.bot_id === "undefined"){
-    const matches = data.text.match(chkRegExp);
-    if(matches !== null && matches.length){
-      for(const match of matches){
-        bot.postMessage(data.channel, redmineUrl + match.replace("#", ""), {
-          icon_emoji: ":panda_face:"
+  if (data.type === "message" && typeof data.bot_id === "undefined") {
+    const matches = data.text.match(/#[0-9]{4,10}/gmi);
+    if (matches !== null && matches.length) {
+      for (const match of matches) {
+        const url = process.env.REDMINE_URL + match.replace("#", "");
+        request({
+          url: url + ".json",
+          headers: {
+            "X-Redmine-API-Key": process.env.REDMINE_TOKEN
+          }
+        }, (error, response, body) => {
+          if (!error && response.statusCode == 200) {
+            const issue = JSON.parse(body).issue;
+            if(typeof issue !== "undefined"){
+              let msg = `<${url}|${issue.subject}>`;
+              if(typeof issue.assigned_to.name !== "undefined"){
+                msg += `\nAssigned to: ${issue.assigned_to.name}`;
+              }
+              if(typeof issue.status.name !== "undefined"){
+                msg += `\nStatus: ${issue.status.name}`;
+              }
+              bot.postMessage(data.channel, msg, {
+                icon_emoji: ":panda_face:"
+              });
+            }
+          }
         });
       }
     }
   }
 });
 
-// handle heroku requests to keep the process alive
+// Handle heroku requests to keep the process alive
 app.get("/", (req, res) => {
   res.send("Bot is running...")
 });
-app.listen(port);
+app.listen(process.argv.slice(2)[0]);
