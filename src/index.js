dafba65f5d6edd3ee1d3cf553554085de157a4ad
Julian Motz <me@julianmotz.com>
2017-04-29 13:12:17 +0200

    handle heroku requests to keep the process alive

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-const SlackBot = require("slackbots");
+const SlackBot = require("slackbots"),
+    express = require("express");
 
+// Init instances
 const bot = new SlackBot({
     token: process.env.SLACK_TOKEN,
     name: "Redmine Bot"
   }),
-  redmineUrl = "https://redmine.cando-image.com/issues/",
-  chkRegExp = /#[0-9]{4,10}/gmi;
+  app = express();
 
+// settings
+const redmineUrl = "https://redmine.cando-image.com/issues/",
+  chkRegExp = /#[0-9]{4,10}/gmi,
+  port = process.argv.slice(2)[0];
+
+// listen on slack messages
 bot.on("message", function(data) {
   if(data.type === "message" && typeof data.bot_id === "undefined"){
     const matches = data.text.match(chkRegExp);
@@ -19,3 +26,9 @@ bot.on("message", function(data) {
     }
   }
 });
+
+// handle heroku requests to keep the process alive
+app.get("/", (req, res) => {
+  res.send("Bot is running...")
+});
+app.listen(port);
